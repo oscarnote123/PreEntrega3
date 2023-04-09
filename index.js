b1ba12bc78d00843c4fd50d5d0d33264ba1b93a6
oscarnote123 <122924668+oscarnote123@users.noreply.github.com>
2023-04-09 09:58:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-/*
-const historialInscripciones = JSON.parse(sessionStorage.getItem("historialInscripciones")) || [];
+// Declaración de una constante "historialInscripciones" que obtiene su valor del local storage
+const historialInscripciones = JSON.parse(localStorage.getItem("historialInscripciones")) || [];
 
+// Función que borra los campos del formulario
 function borrarFormulario() {
   const inputs = document.querySelectorAll("input[type='text'], input[type='date']");
   inputs.forEach(input => input.value = "");
   document.querySelector("#duracion").value = "";
 }
 
-
+// Función que muestra el historial de inscripciones en la página y actualiza en el local storage
 function mostrarHistorialInscripciones() {
   const lista = document.querySelector("#historial");
   lista.innerHTML = "";
@@ -20,12 +21,14 @@ function mostrarHistorialInscripciones() {
 
   localStorage.setItem("historialInscripciones", JSON.stringify(historialInscripciones));
 }
-
+// declaro constante mensaje
 const mensaje = document.querySelector("#mensaje");
 const formulario = document.querySelector("#formulario");
 
 formulario.addEventListener("submit", (event) => {
   event.preventDefault();
+  
+
   const nombre = document.querySelector("#nombre").value;
   const apellido = document.querySelector("#apellido").value;
   const edad = parseInt(document.querySelector("#edad").value);
@@ -46,6 +49,7 @@ formulario.addEventListener("submit", (event) => {
   mensaje.textContent = resultado;
 });
 
+// Función que calcula el precio total en función del instrumento y los días de reserva
 function calcularPrecioTotal(instrumento, diasReserva) {
   let precio;
   switch (instrumento) {
@@ -67,11 +71,20 @@ function calcularPrecioTotal(instrumento, diasReserva) {
   }
   return precio * diasReserva;
 }
-
-function inscribirseEnMusica(nombre, apellido, edad, instrumento, fechaInicio) {
-  if (edad < 18) {
+   //Función que se encarga de inscribir al usuario en la escuela de música 
+  function inscribirseEnMusica(nombre, apellido, edad, instrumento, fechaInicio) {
+    if (edad < 18) {
     return "Lo sentimos, debes ser mayor de edad para inscribirte.";
   } else {
+    // Validar que el nombre y apellido no tenga números, uso una expresion regular de JS
+    const regex = /\d/;
+    if (regex.test(nombre)) {
+      return "El nombre no puede contener números.";
+    }
+        
+    if (regex.test(apellido)) {
+      return "El apellido no puede contener números.";
+    }
     const diasReserva = Math.ceil((new Date(fechaInicio) - new Date()) / (1000 * 60 * 60 * 24));
     const precioTotal = calcularPrecioTotal(instrumento, diasReserva);
     const mensaje = `¡Felicidades ${nombre} ${apellido}! Te has inscripto en la escuela de música para tocar el ${instrumento}. El precio total de tu reserva es de ${precioTotal} dólares.`;
@@ -83,111 +96,13 @@ function inscribirseEnMusica(nombre, apellido, edad, instrumento, fechaInicio) {
       fechaInicio
     };
     historialInscripciones.push(inscripcion);
-    mostrarHistorialInscripciones();
+    mostrarHistorialInscripciones();// Actualiza el historial en la página
     return mensaje;
   }
-  
+  //borrar campos
+}
+function borrarCampos() {
+  document.querySelector("#formulario").reset();
+  document.querySelector("#mensaje").textContent = "";
 }
 
-*/
-
-
-//segundo codigo con promise
-const historialInscripciones = JSON.parse(localStorage.getItem("historialInscripciones")) || [];
-
-function borrarFormulario() {
-  const inputs = document.querySelectorAll("input[type='text'], input[type='date']");
-  inputs.forEach(input => input.value = "");
-  document.querySelector("#duracion").value = "";
-}
-
-function mostrarHistorialInscripciones() {
-  const lista = document.querySelector("#historial");
-  lista.innerHTML = "";
-
-  historialInscripciones.forEach(inscripcion => {
-    const item = document.createElement("li");
-    item.textContent = `${inscripcion.nombre} ${inscripcion.apellido} - ${inscripcion.instrumento} - ${inscripcion.edad} años - ${inscripcion.fechaInicio}`;
-    lista.appendChild(item);
-  });
-
-  localStorage.setItem("historialInscripciones", JSON.stringify(historialInscripciones));
-}
-
-const mensaje = document.querySelector("#mensaje");
-const formulario = document.querySelector("#formulario");
-
-formulario.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const nombre = document.querySelector("#nombre").value;
-  const apellido = document.querySelector("#apellido").value;
-  const edad = parseInt(document.querySelector("#edad").value);
-  const instrumento = document.querySelector("#instrumento").value;
-  const fechaInicio = document.querySelector("#fechaInicio").value;
-
-  if (!instrumento) {
-    mensaje.textContent = "Seleccione un instrumento.";
-    return;
-  }
-
-  if (!fechaInicio) {
-    mensaje.textContent = "Seleccione una fecha de inicio.";
-    return;
-  }
-
-  const resultado = await inscribirseEnMusica(nombre, apellido, edad, instrumento, fechaInicio);
-  mensaje.textContent = resultado;
-});
-
-function calcularPrecioTotal(instrumento, diasReserva) {
-  let precio;
-  switch (instrumento) {
-    case 'saxo':
-      precio = 10;
-      break;
-    case 'piano':
-      precio = 15;
-      break;
-    case 'guitarra':
-      precio = 12;
-      break;
-    case 'clarinete':
-      precio = 8;
-      break;
-    default:
-      precio = 0;
-      break;
-  }
-  return precio * diasReserva;
-}
-
-async function inscribirseEnMusica(nombre, apellido, edad, instrumento, fechaInicio) {
-  if (edad < 18) {
-    return "Lo sentimos, debes ser mayor de edad para inscribirte.";
-  } else {
-    const diasReserva = Math.ceil((new Date(fechaInicio) - new Date()) / (1000 * 60 * 60 * 24));
-    const precioTotal = calcularPrecioTotal(instrumento, diasReserva);
-    const mensaje = `¡Felicidades ${nombre} ${apellido}! Te has inscrito en la escuela de música para tocar el ${instrumento}. El precio total de tu reserva es de ${precioTotal} dólares.`;
-    const inscripcion = {
-      nombre,
-      apellido,
-      edad,
-      instrumento,
-      fechaInicio
-    };
-    historialInscripciones.push(inscripcion);
-    mostrarHistorialInscripciones();
-
-    await esperaAleatoria(); // Espera aleatoria simulando una operación asíncrona
-
-    return mensaje;
-  }    
-}
-
-function esperaAleatoria() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve();
-    }, Math.floor(Math.random() * 5000));
-  });
-}
